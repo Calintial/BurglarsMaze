6bc30c31102c7f14564fe157150373c504574297
Víctor Guzmán <fawques@gmail.com>
2013-09-19 09:55:50 +0200

    Eliminado lib de la carpeta del proyecto. Tiene que estar en el compilador nada más

--- lib/examples/004 - Sprites Tile Map Mode 1 (Sprite Rotation)/make.bat
+++
@@ -1,1 +0,0 @@
-zcc +cpc TileMapConf.asm -create-app -make-app -O3 -unsigned -o uno.bin code.c -lcpcrslib -lndos -zorg=16384
--- lib/examples/007 - Pucrunch/make.bat
+++
@@ -1,4 +0,0 @@
-zcc +cpc  -create-app -make-app -O3 -unsigned -o uno.bin code.c -lcpcrslib -lndos -zorg=16384
-
-copy uno.bin e:\Raul\WinAPE
-
